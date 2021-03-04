bcb7abbf01fb857f11f21bd6fd120840dbe3312e
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-04 14:04:05 +0900

    問19 include?メソッドの使用方法

--- ruby1.rb
+++ ruby1.rb
@@ -252,17 +252,31 @@
 
 # 問18 配列内の偶数値のみを取り出す
 
-def count_evens(array)
-  i = 0
-    array.each do |num|
-      if num.even?
-       i += 1
-      end
-    end
-    p i
+# def count_evens(array)
+#   i = 0
+#     array.each do |num|
+#       if num.even?
+#        i += 1
+#       end
+#     end
+#     p i
+# end
+
+
+# count_evens([2, 1, 2, 3, 4])
+# count_evens([2, 2, 0])
+# count_evens([1, 3, 5])
+
+# 問19 配列からの値の取り出し include? メソッド
+
+def array123(array)
+  if array.include?(1) && array.include?(2) && array.include?(3)
+    p true
+  else
+    p false
+  end
 end
 
-
-count_evens([2, 1, 2, 3, 4])
-count_evens([2, 2, 0])
-count_evens([1, 3, 5])
+array123([1, 1, 2, 3, 1])
+array123([1, 1, 2, 4, 1])
+array123([1, 1, 2, 1, 2, 3])
