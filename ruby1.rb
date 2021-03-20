7c00ff6c8ad540d287412b8fedfd0a9996fdd729
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-20 20:03:28 +0900

    問30　配列のバイナリーサーチ

--- ruby1.rb
+++ ruby1.rb
@@ -430,19 +430,51 @@
 
 # 問29 API問題 重複する値を除外する
 
-def lone_sum(array)
-  ary = []
-  array.each do |num|
-    i = 0
-    unless num == ary[i]
-      ary << num
-    end
-    i += 1
-  end
+# def lone_sum(array)
+#   ary = []
+#   array.each do |num|
+#     i = 0
+#     unless num == ary[i]
+#       ary << num
+#     end
+#     i += 1
+#   end
 
-  puts ary.sum
+#   puts ary.sum
+# end
+
+# lone_sum([1, 2, 3])
+# lone_sum([3, 2, 3])
+# lone_sum([3, 3, 3])
+
+# 問30 配列の検索について バイナリーサーチ
+
+def binary_search(array, number_of_elements, target)
+  left = 0
+  right = number_of_elements - 1
+  while left <= right
+    center = (left + right) / 2
+    if array[center] == target
+      return center
+    elsif array[center] < target
+      left = center + 1
+    else
+      right = center - 1
+    end
+  end
+  return -1 
 end
 
-lone_sum([1, 2, 3])
-lone_sum([3, 2, 3])
-lone_sum([3, 3, 3])
+array=[1,3,5,6,9,10,13,20,26,31]
+
+puts "検索したい数字を入力してください"
+target = gets.to_i
+number_of_elements = array.length
+
+result = binary_search(array, number_of_elements, target)
+
+if result == -1
+  puts "#{target}は配列内に存在しません"
+else
+  puts "#{target}は配列の#{result}番目に存在します "
+end
