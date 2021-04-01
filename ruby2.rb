31668a59bd6add2796ec8b3b536b85c68b8fd6b2
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-04-01 17:13:35 +0900

    文字列のindexを指定して再代入

--- ruby2.rb
+++ ruby2.rb
@@ -20,18 +20,18 @@
      # p array.any? #中身があればtrue ,なければfalse
 
 # # 配列の値に該当の値はあるかの検索
-    array = ["ruby", "python", "react", "java"]
-    puts array.include?("python")  # true,falseで返す
+#     array = ["ruby", "python", "react", "java"]
+#     puts array.include?("python")  # true,falseで返す
      
 # # 配列内の文字列を結合
-     array = ["H","E","L","L","O"]
-     puts array.join       #各配列の文字列をそのまま連結
-     puts array.join(" ")  #各配列の文字列に空白で区切りを行う
+     # array = ["H","E","L","L","O"]
+     # puts array.join       #各配列の文字列をそのまま連結
+     # puts array.join(" ")  #各配列の文字列に空白で区切りを行う
 
 
 # #配列内の数字を計算
-     array = [2,3,4,5,6,7,8,9]
-     p array.map{|i| i * 2}    # ブロック|i|に 引数の計算結果が代入される
+     # array = [2,3,4,5,6,7,8,9]
+     # p array.map{|i| i * 2}    # ブロック|i|に 引数の計算結果が代入される
      
 # # # 配列同士の重複した値を取り出す
 #    array1 = [ 1, 3, 55, 66, 12]
@@ -40,14 +40,16 @@
 
 # # 文字列の抽出
 #   # 文字列のみ
-#     str = "Hello"
-#     p str[-2, 2]  # -1は最後から算出
+    str = "Hello"
+    p str[-2, 2] # -1は最後から算出
+    p str[-1] = "L"   # 文字列indexの再代入
+    p str   
 
 #   # sliceメソッド !をつけると破壊メソッド
 #   p str.slice(-2, 2)
 
 #   # 文字列を配列に格納
-  p "abcdifg".chars # 一文字ずつ配列に格納
+#   p "abcdifg".chars # 一文字ずつ配列に格納
 
     # 文字列を数字に変換して配列に格納
    #  p "123456789".chars.map( &:to_i)
@@ -86,12 +88,12 @@ p time.strftime(" %Y年 %m月 %d日 %H時 %M分 %S秒 ") # strftime("%~で手軽
 
 # 小数点の表示
 
-num = 15.to_f + 10  # .to_f で小数点表示
+# num = 15.to_f + 10  # .to_f で小数点表示
 
-p sprintf("%.3f",num)    # sprintf("%.3f", num) で小数点第三位までを表示
+# p sprintf("%.3f",num)    # sprintf("%.3f", num) で小数点第三位までを表示
 
 # splitメソッド
 
-puts "-----標準入力 splitメソッドの使用方法"
-num = gets 
-p num.split # 標準入力時の 空白・改行を削除して配列に格納
+# puts "-----標準入力 splitメソッドの使用方法"
+# num = gets 
+# p num.split # 標準入力時の 空白・改行を削除して配列に格納
